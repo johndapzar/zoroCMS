797b516a9dd0d6cfbde05177a459caa98cdd4a18
John Dapzar <johndapzar@yahoo.com>
2015-02-23 13:52:25 +0530

    Migration

---
+++ database/migrations/2015_02_23_081142_create_photo_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photo', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('album_id');
+			$table->string('caption');
+			$table->string('file');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('photo');
+	}
+
+}
